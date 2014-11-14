f5c3b6a81605cc1c3388622d3431df275191ba27
Jiri Vrany <jiri.vrany@tul.cz>
2014-11-14 10:30:25 +0100

    add python mysql support

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='MTBOStats',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      description='MTBO Statistic of world class races',
+      author='Jiri Vrany',
+      author_email='jiri.vrany@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
+     install_requires=['Flask>=0.10.1', 'MySQL-python', 'Flask-MySQL'],
      )
