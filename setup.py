805938f2bf54265dbc80e7838a2b72ac06d20697
Jiri Vrany <jiri.vrany@tul.cz>
2017-09-26 11:00:05 +0200

    cleanup for transfer to py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='MTBOStats',
-      version='1.0',
+      version='1.1',
       description='MTBO Statistic of world class races',
       author='Jiri Vrany',
       author_email='jiri.vrany@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1', 'MySQL-python>=1.2.5', 'Flask-MySQL>=1.2'],
+      url='https://github.com/jirivrany/stats_mtbo',
+     install_requires=['Flask==0.12.2', 'PyMySQL==0.7.11', 'Flask-MySQL==1.4.0'],
      )
